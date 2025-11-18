ea2f2f981ac4a1ab5d15e6c812152627130ba3f9
annae0sang3 <annae0sang3@gmail.com>
2025-11-19 04:45:13 +0900

    Delete notifier-worker-backend/src/index.d.ts

--- notifier-worker-backend/src/index.d.ts
+++
@@ -1,8 +0,0 @@
-export interface Env {
-    SETTINGS_KV?: KVNamespace;
-}
-declare const _default: {
-    fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response>;
-};
-export default _default;
-//# sourceMappingURL=index.d.ts.map
